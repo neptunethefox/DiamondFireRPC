334553f1991c23609399b71abd70fd47b7e6ba34
neptunethefox <neptunethefox@proton.me>
2025-09-02 13:58:40 +0800

    chore: update README.md
    feat: custom plot rpc woo!!!!!

---
+++ src/main/java/io/github/neptunethefox/dfrpc/plot/PlotRPC.java
@@ -0,0 +1,111 @@
+package io.github.neptunethefox.dfrpc.plot;
+
+import dev.dfonline.flint.Flint;
+import dev.dfonline.flint.feature.impl.PacketLoggerFeature;
+import dev.dfonline.flint.util.Logger;
+import dev.dfonline.flint.util.result.EventResult;
+import io.github.neptunethefox.dfrpc.DiamondFireRPC;
+import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.s2c.play.OverlayMessageS2CPacket;
+import net.minecraft.text.PlainTextContent;
+import net.minecraft.text.Text;
+import net.minecraft.text.TextColor;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class PlotRPC extends PacketLoggerFeature {
+
+    public static boolean active = false;
+    public static String details = "";
+    public static String state = "";
+
+    public static String largeImage = "plot";
+    public static String largeImageText = "";
+
+    private static final Logger LOGGER = Logger.of(PlotRPC.class);
+
+    public void reset() {
+        largeImage = "plot";
+        largeImageText = "";
+        details = "";
+        state = "";
+        active = false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return DiamondFireRPC.CONFIG.ALLOW_PLOT_CONTROL();
+    }
+
+    @Override
+    public EventResult onReceivePacket(Packet<?> packet) {
+        if (Flint.getUser().getPlot() == null) {
+            reset();
+            return EventResult.PASS;
+        }
+        if (!DiamondFireRPC.CONFIG.ALLOW_PLOT_CONTROL()) return EventResult.PASS;
+
+        if (packet instanceof OverlayMessageS2CPacket(Text text)) {
+            if (Text.empty().contains(text)) {
+                return EventResult.PASS;
+            }
+
+            var siblings = text.getSiblings();
+            if (siblings.isEmpty()) {
+                return EventResult.PASS;
+            }
+
+            var command = siblings.getFirst();
+            if (!Objects.equals(command.getStyle().getColor(), TextColor.fromRgb(0xFF9D14))) {
+                return EventResult.PASS;
+            }
+
+            if (command.getContent().equals(new PlainTextContent.Literal("activate"))) {
+                active = true;
+                return EventResult.CANCEL;
+            }
+
+            if (command.getContent().equals(new PlainTextContent.Literal("deactivate"))) {
+                reset();
+                return EventResult.CANCEL;
+            }
+
+            if (command.getContent().equals(new PlainTextContent.Literal("details"))) {
+                boolean first = true;
+                var textBuilder = new ArrayList<String>();
+
+                for (var part : siblings) {
+                    if (first) {
+                        first = false;
+                        continue;
+                    }
+
+                    textBuilder.add(part.getString());
+                }
+                details = String.join(" ", textBuilder);
+                return EventResult.CANCEL;
+            }
+
+            if (command.getContent().equals(new PlainTextContent.Literal("state"))) {
+                boolean first = true;
+                var textBuilder = new ArrayList<String>();
+
+                for (var part : siblings) {
+                    if (first) {
+                        first = false;
+                        continue;
+                    }
+
+                    textBuilder.add(part.getString());
+                }
+                state = String.join(" ", textBuilder);
+                return EventResult.CANCEL;
+            }
+
+            return EventResult.PASS;
+        }
+
+        return EventResult.PASS;
+    }
+}
